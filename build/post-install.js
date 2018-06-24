364ca653578f39862f70dd7d2d76046d64888835
Anton <anton@adc.sh>
2018-06-24 02:49:38 +0100

    postinstall

---
+++ build/post-install.js
@@ -0,0 +1,3 @@
+"use strict";
+
+console.log('Installed successfully, now clone the package and use yarn link bestie to use it from the local env.');
---
+++ src/post-install.js
@@ -0,0 +1,1 @@
+console.log('Installed successfully, now clone the package and use yarn link bestie to use it from the local env.')
